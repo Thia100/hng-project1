5047d55f64cf4379b8de9dfa7eb42e106aaef911
Thia100 <thegbolahanfathia@gmail.com>
2025-10-18 02:43:55 +0100

    final commit

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const time = document.querySelector("#time");
 function updateTime() {
   time.textContent = Date.now();
 }
-updateTime(); 
-setInterval(updateTime, 1000); 
+updateTime();
+setInterval(updateTime, 1000);
